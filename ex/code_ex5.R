7e0367843b85eae477b01fd206e97f0ce710b851
Benjamin Cretois <benjamcr@ntnu.no>
2020-10-02 09:24:12 +0200

    Add Lab 6

--- ex/code_ex5.R
+++ ex/code_ex5.R
@@ -3,9 +3,9 @@ library(tidyverse)
 openintro::evals
 data <- openintro::evals
 
-ggplot(data, aes(x=score)) + geom_histogram()
+ggplot(data, aes(x=score)) + geom_histogram() + theme_bw()
 ggplot(data, aes(y=score, x= bty_avg)) + geom_point() + geom_smooth(method = 'lm')
-ggplot(data, aes(y=score, x= pic_outfit)) + geom_boxplot() 
+ggplot(data, aes(y=score, x= pic_outfit)) + geom_boxplot() + theme_bw ()
 
 data %>% 
   group_by(pic_outfit) %>% 
@@ -22,8 +22,12 @@ ggplot(data, aes(y=score, x= bty_avg)) + geom_point() + geom_jitter()
 lm1 <- lm(score ~ bty_avg, data = data)
 summary(lm1)
 
-pred_df <- data.frame(score_pred = predict(lm1, data), data$bty_avg)
+broom::tidy(lm1)
+seq_btyavg <- seq(from = min(data$bty_avg), to = max(data$bty_avg), length.out = 463)
 
-ggplot(data, aes(y=score, x= bty_avg)) + 
-  geom_point() +
-  geom_line(color = 'red', data = pred_df, aes(x=data.bty_avg, y = score_pred)) + geom_smooth(method = 'lm', se = FALSE)
+data$score_pred <- predict(lm1, data) 
+
+ggplot(data, aes(x= bty_avg, y = score)) + 
+  geom_point() + 
+  geom_jitter() +
+  geom_line(aes(y = score_pred), color = 'red')
